0d4222090d238c86b485fbbc2bf720fc88b76a04
Alby Tomy <78116411+alby-tomy@users.noreply.github.com>
2024-03-06 14:55:00 +0530

    Update Q1.py

--- Challenges/Challenge1/Q1.py
+++ Challenges/Challenge1/Q1.py
@@ -1,17 +1,18 @@
 listMov = ["Manichithrathazhu","CID Moosa","Parakkumthalika","Vettam","Meeshamadhavan","Lelam","Thengashipattanam","Spadikam"]
 print(listMov)
 
-#removing 5th element
-print(listMov.pop(4))
+# Removing 5th element
+removed_element = listMov.pop(4)
+print(removed_element)
 
-#inserting element at 4th index
-listMov.insert(3,"Harry Potter")
+# Inserting element at 4th index
+listMov.insert(4, "Harry Potter")  
 
 print(listMov)
 
-#printing 4th element
-print(listMov[3])
+# Printing 4th element
+print(listMov[3])  
 
-#appending another value to the list
+# Appending another value to the list
 listMov.append("Classmates")
 print(listMov)
