8a4797f15afcaa0b873216b92b437340c011d041
alby-tomy <alby.u.tomy@gmail.com>
2024-05-09 03:00:01 +0530

    -

--- python_preparation/interview_preparation/fibonacci_pyramid.py
+++ python_preparation/interview_preparation/fibonacci_pyramid.py
@@ -1,11 +1,23 @@
-def fibonacci_pyramid(rows):
-    a,b = 0,1
-    for i in range(rows):
-        print(" "* (rows-i), end="")
+# def fibonacci_pyramid(rows):
+#     a,b = 0,1
+#     for i in range(rows):
+#         print(" "* (rows-i), end="")
         
-        for j in range(i+1):
-            print(a, end=" ")
-            a,b, = b,a+b
-        print()
+#         for j in range(i+1):
+#             print(a, end=" ")
+#             a,b, = b,a+b
+#         print()
         
-fibonacci_pyramid(5)
+# fibonacci_pyramid(5)
+
+# recurssion method
+def fabi_rec(nu):
+    if nu <= 1:
+        return 1
+    else:
+        return fabi_rec(nu-1) + fabi_rec(nu-2)
+    
+num_r = 10
+for i in range(num_r):
+    print(fabi_rec(i), end=" ")
+print()
